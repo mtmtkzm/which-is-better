491c525be15f854b94d937c605f7993bdc81cf4f
mtmtkzm <hello@mtmtkzm.com>
2018-04-28 19:29:55 +0900

    デフォの Example を App カラ呼び出し

---
+++ laravel/resources/assets/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <Example></Example>
+  </div>
+</template>
+<script>
+  import Example from "./components/Example";
+
+  export default {
+    components: {
+      Example
+    }
+  };
+</script>
