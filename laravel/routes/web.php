398f3ee40ba09fc7e418432e38b6a8556c61ed6f
mtmtkzm <hello@mtmtkzm.com>
2018-05-01 21:24:36 +0900

    とりえあず最後までやったがエラー

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,5 +13,33 @@
 */
 
 Route::get('/', function () {
-    return view('app');
+  $tasks = Task::orderBy('created_at', 'asc')->get();
+
+  return view('tasks', [
+    'tasks' => $tasks
+  ]);
+});
+
+Route::post('/task', function (Request $request) {
+  $validator = Validator::make($request->all(), [
+    'name' => 'required|max:255',
+  ]);
+
+  if ($validator->fails()) {
+    return redirect('/')
+      ->withInput()
+      ->withErrors($validator);
+  }
+
+  $task = new Task;
+  $task->name = $request->name;
+  $task->save();
+
+  return redirect('/');
+});
+
+Route::delete('/task/{task}', function (Task $task) {
+  $task->delete();
+
+  return redirect('/');
 });
