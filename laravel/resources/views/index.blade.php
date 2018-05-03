1764eb57aa777800e2241611063bfa3d258d9cd1
mtmtkzm <hello@mtmtkzm.com>
2018-05-03 16:36:16 +0900

    ここでいったんSPAじゃないのはおわり

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -1,34 +1,32 @@
 @extends('layouts.app')
 @section('content')
-    <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    New Task
-                </div>
+  <div class="container">
+    <div class="col-sm-offset-2 col-sm-8">
+      <div class="panel panel-default">
+        <div class="panel-heading">New Task</div>
 
-                <div class="panel-body">
-                    <!-- Display Validation Errors -->
-                @include('common.errors')
+        <div class="panel-body">
+          <!-- Display Validation Errors -->
+        @include('common.errors')
 
-                <!-- New Task Form -->
-                    <form action="{{ url('add_task')  }}" method="POST" class="form-horizontal">
-                    {{ csrf_field() }}
+        <!-- New Task Form -->
+          <form action="{{ url('add_task')  }}" method="POST" class="form-horizontal">
+          {{ csrf_field() }}
 
-                    <!-- Task Name  -->
-                        <div class="form-group">
-                            <label class="col-sm-3 control-label">Task</label>
-                            <div class="col-sm-6">
-                                <input type="text" name="name" class="form-control" placeholder="name">
-                                <input type="text" name="than" class="form-control" placeholder="than">
-                            </div>
-                            <button type="submit" class="btn btn-default fa fa-btn fa-plus"></button>
-                        </div>
-
-                    </form>
-                </div>
+          <!-- Task Name  -->
+            <div class="form-group">
+              <label class="col-sm-3 control-label">Task</label>
+              <div class="col-sm-6">
+                <input type="text" name="name" class="form-control" placeholder="name">
+                <input type="text" name="than" class="form-control" placeholder="than">
+              </div>
+              <button type="submit" class="btn btn-default fa fa-btn fa-plus"></button>
             </div>
+
+          </form>
         </div>
+      </div>
     </div>
+  </div>
 
 @endsection
--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -3,10 +3,7 @@
 <head>
     <title>Laravel Quickstart - Basic</title>
 
-    <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-
-    <!-- Styles -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet'>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
@@ -23,7 +20,5 @@
 
 @yield('content')
 
-<!-- JavaScripts -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </body>
 </html>
